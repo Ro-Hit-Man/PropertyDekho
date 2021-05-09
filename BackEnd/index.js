f313a2563581372062eba8b73395f5dc00e28bf8
Ro-Hit-Man <rohitmanmittal264@gmail.com>
2021-05-09 15:46:38 +0530

    BackEnd uploaded by Rohit Mittal

---
+++ BackEnd/index.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+
+var app = express();
+app.use(cors());
+
+var client = new MongoClient('mongodb+srv://myhome:myhome@cluster0.zj3m5.mongodb.net/myhome?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true});
+var connection;
+client.connect((err,db)=>{
+    if(!err){
+        connection=db;
+        console.log("Database Connected Successfully");
+    }
+    else{
+        console.log(err);
+    }
+});
+
+app.get('/listProperty',(req,res)=>{
+    var propertyCollection = connection.db('myhome').collection('property');
+    propertyCollection.find().toArray((err,docs)=>{
+        if(!err){
+            res.send({status:"ok",data:docs});
+        }
+        else{
+            res.send({status:"failed",data:err});
+        }
+    });
+});
+
+app.get('/detailProperty',(req,res)=>{
+    var propertyCollection = connection.db('myhome').collection('property');
+    propertyCollection.find({_id:ObjectID(req.query.id)}).toArray((err,docs)=>{
+        if(!err){
+            res.send({status:"ok",data:docs});
+        }
+        else{
+            res.send({status:"failed",data:err});
+        }
+    });
+});
+
+// app.get('/searchProperty',(req,res)=>{
+//     var propertyCollection = connection.db('myhome').collection('property');
+//     propertyCollection.find().toArray((err,docs)=>{
+//         if(!err){
+//             res.send({status:"ok",data:docs});
+//         }
+//         else{
+//             res.send({status:"failed",data:err});
+//         }
+//     });
+// });
+
+app.post('/postProperty', bodyParser.json(), (req,res)=>{
+    var propertyCollection = connection.db('myhome').collection('property');
+    propertyCollection.insert(req.body , (err,result)=>{
+        if(!err){
+            res.send({status:"ok",data:"Property Posted Succesfully"});
+        }
+        else{
+            res.send({status:"failed",data:err});
+        }
+    });
+});
+
+app.post('/registerUser', bodyParser.json(), (req,res)=>{
+    var userCollection = connection.db('myhome').collection('user');
+    userCollection.insert(req.body , (err,result)=>{
+        if(!err){
+            res.send({status:"ok",data:"User Registered Succesfully"});
+        }
+        else{
+            res.send({status:"failed",data:err});
+        }
+    });
+});
+
+app.get('/checkUser', (req,res)=>{
+    var userCollection = connection.db('myhome').collection('user');
+    userCollection.find({email: req.query.email}).toArray((err,docs)=>{
+        if(!err){
+            res.send({status:"ok",data:docs});
+        }
+        else{
+            res.send({status:"failed",data:err});
+        }
+    })
+});
+
+app.listen(3000,()=>{
+    console.log("Server is listing at port 3000");
+});
--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
+import axios from 'axios'
 
 export class Register extends Component {
 
@@ -15,7 +16,6 @@ export class Register extends Component {
             buyer:false,
             owner:false,
             broker:false,
-            isValid:true,
             passwordRegex : /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/,
             emailRegex : /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/
         }
@@ -29,39 +29,54 @@ export class Register extends Component {
     {
         this.setState({[e.target.id]:e.target.checked});
     }
-    printData()
-    {
-        alert(this.state.buyer + '---' + this.state.owner + '---' + this.state.broker);
-    }
 
-    validate()
+    register()
     {
         if(this.state.name == "" || this.state.name == null || this.state.name.length == 0){
-            this.state.isValid = false;
             document.getElementById('name').style.border = "3px solid red";
             alert('Name cannot be Empty');
         }
-        if(!this.state.passwordRegex.test(this.state.password)){
-            this.state.isValid = false;
+       else if(!this.state.passwordRegex.test(this.state.password)){
             document.getElementById('password').style.border = "3px solid red";
             alert('Password must contain 1 lower case, 1 upper case, 1 digit, 1 special character, length atleast 8');
         }
-        if(this.state.repassword == "" || this.state.password != this.state.repassword){
-            this.state.isValid = false;
-            document.getElementById('repassword').style.border = "3px solid red";
-            alert('Password did not match');
-        }
-        if(!this.state.emailRegex.test(this.state.email) || this.state.email == ""){
-            this.state.isValid = false;
-            document.getElementById('email').style.border = "3px solid red";
-            alert('Enter valid email address');
-        }
-        if(this.state.number.length<10 || this.state.number.length>10 || this.state.number == "" || this.state.number == null){
-            this.state.isValid = false;
-            document.getElementById('number').style.border = "3px solid red";
-            alert('Enter valid mobile number');
-        }
-        return this.state.isValid;
+            else if(this.state.repassword == "" || this.state.password != this.state.repassword){
+                document.getElementById('repassword').style.border = "3px solid red";
+                alert('Password did not match');
+            }
+                else if(!this.state.emailRegex.test(this.state.email) || this.state.email == ""){
+                    document.getElementById('email').style.border = "3px solid red";
+                    alert('Enter valid email address');
+                }
+                     else if(this.state.number.length<10 || this.state.number.length>10 || this.state.number == "" || this.state.number == null){
+                            document.getElementById('number').style.border = "3px solid red";
+                            alert('Enter valid mobile number');
+                        }
+                          else if(this.state.buyer == false && this.state.owner == false && this.state.broker == false){
+                                alert('Choose one either buyer, owner or broker')
+                          }
+                            else{
+                                var user = {
+                                    name:this.state.name,
+                                    email:this.state.email,
+                                    password:this.state.password,
+                                    number:this.state.number,
+                                    buyer:this.state.buyer,
+                                    owner:this.state.owner,
+                                    broker:this.state.broker,
+                                }
+                                axios.get('http://localhost:3000/checkUser?email='+user.email).then((res)=>{
+                                    if( res.data.data[0] != undefined){
+                                        alert("User with same Email is already exist");
+                                    }
+                                    else{
+                                        axios.post('http://localhost:3000/registerUser', user).then((res)=>{
+                                            alert(res.data.data);
+                                         });
+                                    }
+                                });
+
+                            }
     }
 
     render() {
@@ -69,7 +84,7 @@ export class Register extends Component {
             <div class='register-container'>
                 <div class='register-wrapper'>
                     <h1>Register</h1>
-                    <form onSubmit={()=>{return this.validate();}}>
+                    <form>
                         <div className='radio-div'>
                             <input type='radio' name='iam' id='buyer' value={this.state.buyer} onChange={(e)=>{this.getDataFromRadio(e);}}></input>
                             <label>Buyer</label>
@@ -80,10 +95,10 @@ export class Register extends Component {
                         </div>
                         <input id='name' placeholder='Enter Name' name='name' value={this.state.name} onChange={(e)=>{this.getDataFromLoginForm(e);}}></input>
                         <input id='email' placeholder='Enter Email' name='email' value={this.state.email} onChange={(e)=>{this.getDataFromLoginForm(e);}}></input>
-                        <input id='password' placeholder='Enter Password' name='password' value={this.state.password} onChange={(e)=>{this.getDataFromLoginForm(e);}}></input>
-                        <input id='repassword' placeholder='Re-Enter Password' name='repassword' value={this.state.repassword} onChange={(e)=>{this.getDataFromLoginForm(e);}}></input>
+                        <input type='password' id='password' placeholder='Enter Password' name='password' value={this.state.password} onChange={(e)=>{this.getDataFromLoginForm(e);}}></input>
+                        <input type='password' id='repassword' placeholder='Re-Enter Password' name='repassword' value={this.state.repassword} onChange={(e)=>{this.getDataFromLoginForm(e);}}></input>
                         <input id='number' placeholder='Enter Mobile Number'name='number' value={this.state.number} onChange={(e)=>{this.getDataFromLoginForm(e);}}></input>
-                        <button>REGISTER</button>
+                        <button type='button' onClick={()=>{this.register();}}>REGISTER</button>
                     </form>
                     <hr/><span>or register using</span>
                     <div class='register-option'>
